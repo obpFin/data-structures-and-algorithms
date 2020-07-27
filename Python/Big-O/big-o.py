da8fdf975d513f21de8a5f7d8de40ec03ce9ba21
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-07-27 12:08:13 +0300

    O(n) and O(1) in Python

--- Python/Big-O/big-o.py
+++ Python/Big-O/big-o.py
@@ -6,9 +6,19 @@ everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt
 large =['nemo'] * 1000  # Runtime increases along with the amount of items
 
 def find_nemo(array):
-  for i in range(len(array)):
-    if array[i] == 'nemo':
-      print('Found Nemo!')
+    for i in range(len(array)):
+        if array[i] == 'nemo':
+            print('Found Nemo!')
 
-find_nemo(everyone)
+def log_first_fish(fishes):
+    print(fishes[0])
+
+
+def log_first_two_fishes(fishes):
+    print(fishes[0])  # O(1)
+    print(fishes[1])  # O(1)
+
+find_nemo(large)  # O(n) --> Linear Time
+log_first_fish(fish)  # O(1) --> Constant Time
+log_first_two_fishes(fish)  # O(1) --> Constant Time
   
