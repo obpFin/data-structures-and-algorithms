72ea7e245de5e6d63509f689f6e5f8667c76e219
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-08-02 10:52:25 +0300

    Log all pairs of array in Python

--- Python/Big-O/excercises.py
+++ Python/Big-O/excercises.py
@@ -2,21 +2,25 @@ def randomFunc():
     return
 
 # What is the Big O of the below function?
+
+
 def funChallenge(input):
-    a = 10 # O(1)
-    a = 50 + 3 # O(1)
+    a = 10  # O(1)
+    a = 50 + 3  # O(1)
     for _ in range(len(input)):  # O(n)
         randomFunc()  # O(n)
         randomVar = True  # O(n)
-    return a # O(1)
+    return a  # O(1)
 
 # BIG O(3 + 4n) --> O(n)
 
 # What is the Big O of the below function?
+
+
 def anotherFunChallenge(input):
     a = 5  # O(1)
-    b = 10 # O(1)
-    c = 50 # O(1)
+    b = 10  # O(1)
+    c = 50  # O(1)
     for i in range(len(input)):
         x = i + 1  # O(n)
         y = i + 2  # O(n)
@@ -29,3 +33,13 @@ def anotherFunChallenge(input):
     whoAmI = "I don't know"  # O(1)
 
 # BIG O(4 + 5n) --> O(n)
+
+# Log all pairs of array
+boxes = ['a', 'b', 'c', 'd', 'e']
+
+def log_all_pairs_of_array(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            print(i, j)
+
+log_all_pairs_of_array(boxes)
