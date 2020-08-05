c4ad4d8387f5281e5255260bc954e863d3777b21
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-08-05 08:11:41 +0300

    Drop non-dominant terms in JS

--- JavaScript/Big-O/rulebook.js
+++ JavaScript/Big-O/rulebook.js
@@ -1,36 +1,58 @@
 // Rule 1: Worst Case
-function findNemo(fish) {   // O(n)
+function findNemo(fish) {
+  // O(n)
   for (let i = 0; i < fish.length; i++) {
-    if (fish[i] === 'nemo') {   // Assume that i is the last item in the list
+    if (fish[i] === 'nemo') {
+      // Assume that i is the last item in the list
       console.log('Found NEMO!');
     }
   }
 }
 
 // Rule 2: Remove Constants
-function printFirstItemThenFirstHalfThenSayHi100Times(items) {    // O(1 + n/2 + 100) -> O(n)
+function printFirstItemThenFirstHalfThenSayHi100Times(items) {
+  // O(1 + n/2 + 100) -> O(n)
   console.log(items[0]);
 
   var middleIndex = Math.floor(items.length / 2);
   var index = 0;
 
   while (index < middleIndex) {
-      console.log(items[index]);
-      index++;
+    console.log(items[index]);
+    index++;
   }
 
   for (var i = 0; i < 100; i++) {
-      console.log('hi');
+    console.log('hi');
   }
 }
 
 // Rule 3: Different terms for inputs
-function compressBoxesTwice(boxes, boxes2) {    // O(a + b)
-  boxes.forEach(box => {
-    console.log(box)
+function compressBoxesTwice(boxes, boxes2) {
+  // O(a + b)
+  boxes.forEach((box) => {
+    console.log(box);
   });
 
-  boxes2.forEach(box => {
-    console.log(box)
+  boxes2.forEach((box) => {
+    console.log(box);
   });
 }
+
+// Rule 4: Drop non-dominant terms
+// O(n + n^2) -> O(n^2)
+function printAllNumbersThenAllPairSums(numbers) {
+  console.log('these are the numbers:');
+  for (number in numbers) {
+    console.log(numbers[number]);
+  }
+
+  console.log('and these are their sums:');
+
+  for (first_number in numbers) {
+    for (second_number in numbers) {
+      console.log(numbers[first_number] + numbers[second_number]);
+    }
+  }
+}
+printAllNumbersThenAllPairSums([1, 2, 3, 4, 5]);
