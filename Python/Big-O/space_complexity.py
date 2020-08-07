95b40ee2e2d64eae6dee2a69472dfe8a9076ba9a
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-08-07 13:34:31 +0300

    space-complexity examples in Python

--- Python/Big-O/space_complexity.py
+++ Python/Big-O/space_complexity.py
@@ -1,6 +1,25 @@
 # What causes Space complexity:
 
 # Variables
-# Data Structures 
-# Function Call 
+# Data Structures
+# Function Call
 # Allocations
+
+
+def booo(n):
+    for _ in range(len(n)):
+        print('boooo!')
+
+
+booo([1, 2, 3, 4, 5])  # O(1)
+
+
+def arrayOfHiNTimes(n):
+    hiArray = []
+    for i in range(len(n)):
+        hiArray[i] = 'hi'  # allocates memory
+    print(hiArray)
+    return hiArray
+
+
+arrayOfHiNTimes(6)  # O(n)
