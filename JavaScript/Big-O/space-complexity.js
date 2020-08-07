bbe6376d76ff4be4aeac7813c57970fe01c0499f
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-08-07 13:34:24 +0300

    space-complexity examples in JS

--- JavaScript/Big-O/space-complexity.js
+++ JavaScript/Big-O/space-complexity.js
@@ -1,6 +1,25 @@
 // What causes Space complexity:
 
 // Variables
-// Data Structures 
-// Function Call 
+// Data Structures
+// Function Call
 // Allocations
+
+function booo(n) {
+  for (let i = 0; i < n.length; i++) {
+    console.log('boooo!');
+  }
+}
+
+booo([1, 2, 3, 4, 5]); // O(1)
+
+function arrayOfHiNTimes(n) {
+  let hiArray = [];
+  for (let i = 0; i < n; i++) {
+    hiArray[i] = 'hi';  // allocates memory
+  }
+  console.log(hiArray)
+  return hiArray;
+}
+
+arrayOfHiNTimes(6); // O(n)
