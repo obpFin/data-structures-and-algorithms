a3c049d9647cdc06e22f77bd38c5d32d63666be9
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-08-15 15:05:06 +0300

    array methods in Python

---
+++ Python/Data-structures/Array/array.py
@@ -0,0 +1,21 @@
+# In computer science, an array data structure, or simply an array, is a data structure consisting of a 
+# collection of elements (values or variables), each identified by at least one array index or key. 
+# An array is stored such that the position of each element can be computed from its index tuple by a 
+# mathematical formula. (https:#en.wikipedia.org/wiki/Array_data_structure)
+
+# 4*4 = 16 bytes of storage
+strings = ['a', 'b', 'c', 'd']
+
+# Access
+strings[2]  # O(1)
+
+# push
+strings.append('e') # O(1)
+
+# pop
+strings.pop(0) # O(1)
+
+# slice
+strings[slice(2, 0)] # O(n)
+
+print(strings)
