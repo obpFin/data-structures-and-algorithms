2a7fef485d38a50e73f61b01096105925b039617
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-08-19 08:30:22 +0300

    hash tables in Python

---
+++ Python/Data-structures/Hash table/hash_table.py
@@ -0,0 +1,26 @@
+# Hash table (hash map) is a data structure that implements an associative array abstract data type,
+# a structure that can map keys to values. A hash table uses a hash function to compute an index,
+# also called a hash code, into an array of buckets or slots, from which the desired value can be found.
+# During lookup, the key is hashed and the resulting hash indicates where the corresponding value is stored.
+# (https://en.wikipedia.org/wiki/Hash_table)
+
+
+def scream():
+    print('ahhhhhhh')
+
+
+user = {
+    "age": 54,
+    "name": 'Kyle',
+    "magic": True,
+    "scream": scream
+}
+
+# access
+user['age']    # O(1)    Hash collision - O(n)
+
+# insert
+user['spell'] = 'Abra kadakabra'   # O(1)
+
+# delete
+del user['spell']   # O(1)
