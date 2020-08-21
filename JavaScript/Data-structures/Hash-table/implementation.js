386754b344f53e50ae841a18abf0634dbe379ffc
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-08-21 08:32:59 +0300

    hashmap keys in js

--- JavaScript/Data-structures/Hash-table/implementation.js
+++ JavaScript/Data-structures/Hash-table/implementation.js
@@ -1,43 +1,57 @@
 class HashTable {
-  constructor(size) {
-    this.data = new Array(size);
+  constructor(size){
+    this.data = new Array(size)
   }
 
-  _hash(key) {    // Very fast, usually O(1)
-    let hash = 0;
-    for (let i = 0; i < key.length; i++) {
-      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+  _hash(key) {
+    let hash = 0
+    for (let i=0; i < key.length; i++){
+        hash = (hash + key.charCodeAt(i) * i) % this.data.length
     }
-    return hash;
+    return hash
   }
 
-  set(key, value) {   // O(1)
-    let address = this._hash(key);
+  set(key, value) {
+    let address = this._hash(key)
 
     // if address space is not free, create linked list
     if (!this.data[address]) {
-      this.data[address] = [];
+      this.data[address] = []
     }
-    this.data[address].push([key, value]);
-    console.log(this.data)
+    this.data[address].push([key, value])
     return this.data
   }
 
-  get(key) {     // O(1) - O(n) if memory collision
-    let address = this._hash(key);
+  get(key){
+    const address = this._hash(key)
     const currentBucket = this.data[address]
     if (currentBucket) {
-      for(let i = 0; i < currentBucket.length; i++) {
-        if (currentBucket[i][0] === key) {
+      for(let i=0; i < currentBucket.length; i++){
+        if(currentBucket[i][0] === key) {
           return currentBucket[i][1]
         }
       }
     }
     return undefined
   }
+  
+  keys(){
+    const keysArray = []
+    console.log(this.data)
+    for (let i=0; i<this.data.length; i++){
+      if(this.data[i]){
+        keysArray.push(this.data[i][0][0])
+      }
+    }
+    return keysArray
+  }
 }
 
-const myHashTable = new HashTable(2);
+const myHashTable = new HashTable(50)
+myHashTable.set('grapes', 10000)
+myHashTable.set('grapes', 10000)
+myHashTable.get('grapes')
+myHashTable.set('apples', 9)
+myHashTable.get('apples')
 
-myHashTable.set('grapes', 243);
-console.log(myHashTable.get('grapes'));
+console.log(myHashTable.keys())
