b97e2954cd0fea607bc4b139ca798e49e84bd5ef
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-09-03 08:20:42 +0300

    started on linked list

---
+++ Python/Data-structures/Linked-list/linked_list.py
@@ -0,0 +1,16 @@
+# Linked list is a linear collection of data elements whose order is not given by their
+# physical placement in memory. Instead, each element points to the next. It is a data structure 
+# consisting of a collection of nodes which together represent a sequence. In its most basic form,
+# each node contains: data, and a reference (in other words, a link) to the next node in the sequence.
+# This structure allows for efficient insertion or removal of elements from any position in the 
+# sequence during iteration. 
+# (https:#en.wikipedia.org/wiki/Linked_list)
+
+basket = ['apples', 'grapes', 'pears']
+
+# linked list: apples -> grapes -> pears
+
+# apples
+# 8934 --> grapes
+#          8392 --> pears
+#                   4020 --> null
