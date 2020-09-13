6df4f076d40da44430ec129a3b51238fab56069d
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-09-13 19:58:55 +0300

    stack implementation with linked list in python

---
+++ Python/Data-structures/Stacks&Queues/stack_implementation.py
@@ -0,0 +1,46 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Stack:
+    def __init__(self):
+        self.top = None
+        self.bottom = None
+        self.length = 0
+
+    def peek(self):
+        return self.top.data
+
+    def push(self, data):
+        new_node = Node(data)
+        if self.bottom == None:
+            self.bottom = new_node
+            self.top = self.bottom
+            self.length = 1
+        else:
+            new_node.next = self.top
+            self.top = new_node
+            self.length += 1
+
+    def pop(self):
+        if not self.top:
+            return None
+        holderPointer = self.top
+        self.top = self.top.next
+        self.length -= 1
+        if self.length == 0:
+            self.bottom = None
+        return holderPointer.data
+
+
+mystack = Stack()
+mystack.push('google')
+mystack.push('udemy')
+mystack.push('stack overflow')
+top = mystack.peek()
+print(top)
+popped_top = mystack.pop()
+print(popped_top)
+
