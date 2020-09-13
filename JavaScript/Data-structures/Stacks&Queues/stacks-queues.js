60d59df4685aff546d82c717ecca181d676e2303
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-09-13 19:55:06 +0300

    added stacks + queues description

---
+++ JavaScript/Data-structures/Stacks&Queues/stacks-queues.js
@@ -0,0 +1,15 @@
+// Queues (https://en.wikipedia.org/wiki/Queue_(abstract_data_type))
+
+// In computer science, a queue is a collection of entities that are maintained in a sequence
+// and can be modified by the addition of entities at one end of the sequence and the removal
+// of entities from the other end of the sequence. By convention, the end of the sequence
+// at which elements are added is called the back, tail, or rear of the queue, and the end
+// at which elements are removed is called the head or front of the queue, analogously to the
+// words used when people line up to wait for goods or services.
+
+// Stacks (https://www.geeksforgeeks.org/queue-data-structure/)
+// A Queue is a linear structure which follows a particular order in which the operations are performed.
+// The order is First In First Out (FIFO). A good example of a queue is any queue of consumers for a 
+// resource where the consumer that came first is served first. The difference between stacks and queues
+// is in removing. In a stack we remove the item the most recently added; in a queue, we remove the item
+// the least recently added.
