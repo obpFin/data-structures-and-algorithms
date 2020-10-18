05ee493b1d5e60a7dd9ea7f5494c7c8146a88a4d
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-10-18 11:44:49 +0300

    bst lookup in JS

--- JavaScript/Data-structures/trees/Binary-search-tree/implementation.js
+++ JavaScript/Data-structures/trees/Binary-search-tree/implementation.js
@@ -36,7 +36,20 @@ class BinarySearchTree {
     }
   }
   lookup(value) {
-
+    if (!this.root) {
+      return false
+    }
+    let currentNode = this.root
+    while(currentNode) {
+      if (value < currentNode.value) {
+        currentNode = currentNode.left
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right
+      } else if (currentNode.value === value) {
+        return currentNode
+      }
+    }
+    return false
   }
 }
 
@@ -49,8 +62,9 @@ tree.insert(9)
 tree.insert(170)
 tree.insert(15)
 tree.insert(1)
+console.log(tree.lookup(6))
 
-console.log(JSON.stringify(traverse(tree.root)))
+// console.log(JSON.stringify(traverse(tree.root)))
 
 
 //        9
