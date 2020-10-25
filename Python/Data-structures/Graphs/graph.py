89d40732561c3fd5b1ccaedd71ee539b530857c9
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-10-25 20:50:19 +0200

    types of graphs in Python

--- Python/Data-structures/Graphs/graph.py
+++ Python/Data-structures/Graphs/graph.py
@@ -10,3 +10,20 @@
 
 # A graph data structure may also associate to each edge some edge value, such as a symbolic label
 # or a numeric attribute (cost, capacity, length, etc.).
+
+# Edge List - each item is an edge in the graph
+graph = [[0,2], [2,3], [2,1], [1,3]]
+
+# Adjacent List - index is the node and value is nodes neighbours
+graph = [[2], [2,3], [0,1,3], [1,2]]
+
+# Adjacent Matrix
+graph = [
+  [0, 0, 1, 0],
+  [0, 0, 1, 1],
+  [1, 1, 0, 1],
+  [0, 1, 1, 0]
+]
+
+#        2   0
+#     1    3
