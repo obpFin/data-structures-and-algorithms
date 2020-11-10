09581fb77e51d6482f20437ca63382c5291a457e
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-11-10 18:56:03 +0200

    linear search in Python

---
+++ Python/Algorithms/Searching/linear_search.py
@@ -0,0 +1,12 @@
+# linear search or sequential search is a method for finding an element within a list. 
+# It sequentially checks each element of the list until a match is found or the whole list has been searched.
+
+# Best O(1)
+# Worst O(n)
+
+beasts = ['Centaur', 'Godzilla', 'Mosura', 'Minotaur', 'Hydra', 'Nessie']
+
+print(beasts.index('Godzilla'))
+
+print('Godzilla' in beasts)
+
