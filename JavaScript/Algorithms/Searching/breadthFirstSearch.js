ee305916cae83eb6df70fabcfac5af99d5f0a73f
Oskari Peltonen <oskari.peltonen@outlook.com>
2021-07-06 20:26:37 +0300

    added bfs/dfs basic explanations

---
+++ JavaScript/Algorithms/Searching/breadthFirstSearch.js
@@ -0,0 +1,10 @@
+// https://en.wikipedia.org/wiki/Breadth-first_search
+
+// Breadth-first search (BFS) is an algorithm for traversing or searching tree or graph data structures.
+// It starts at the tree root (or some arbitrary node of a graph, sometimes referred to as a 'search key'),
+// and explores all of the neighbor nodes at the present depth prior to moving on to the nodes at the next depth level.
+
+// It uses the opposite strategy of depth-first search, which instead explores the node branch as far as possible before being
+// forced to backtrack and expand other nodes.
+
+// In other words, the traversal goes from root level to last depth level, from left to right. - obp
---
+++ JavaScript/Algorithms/Searching/depthFirstSearch.js
@@ -0,0 +1,8 @@
+// https://en.wikipedia.org/wiki/Depth-first_search
+
+// Depth-first search (DFS) is an algorithm for traversing or searching tree or graph data structures.
+// The algorithm starts at the root node (selecting some arbitrary node as the root node in the case of
+// a graph) and explores as far as possible along each branch before backtracking.
+
+// In other words, the traversal goes from root level to as deep possible, until leaf node is found.
+// From there it goes to parent node to check if there's another child for the node and continues the logic until the value is found. - obp
