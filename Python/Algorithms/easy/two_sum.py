3422444d3d61949e72f1da469eb3acc9e587810d
Oskari Peltonen <oskari.peltonen@outlook.com>
2021-08-05 17:33:07 +0300

    two-sum in python

---
+++ Python/Algorithms/easy/two_sum.py
@@ -0,0 +1,18 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+
+# brute force
+def twoSum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[j] == target - nums[i]:
+                return [i, j]
+
+
+result = twoSum([2, 7, 11, 15], 9)
+
+if (result != 9):
+    print('correct')
+else:
+    print('wrong answer')
