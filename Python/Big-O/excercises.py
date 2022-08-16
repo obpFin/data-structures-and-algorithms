5be7de5b2d573cee4d3f74ec667e572cdd3f3da0
Oskari Peltonen <oskari.peltonen@outlook.com>
2022-08-16 14:58:01 +0300

    add greatest_number algo in Python

--- Python/Big-O/excercises.py
+++ Python/Big-O/excercises.py
@@ -34,28 +34,54 @@ def anotherFunChallenge(input):
 
 # BIG O(4 + 5n) --> O(n)
 
+
 # Log all pairs of array
 boxes = ['a', 'b', 'c', 'd', 'e']
+
 
 def log_all_pairs_of_array(arr):
     for i in range(len(arr)):
         for j in range(len(arr)):
             print(i, j)
 
+
 log_all_pairs_of_array(boxes)
 
 # Find 1st, find Nth...
 arr = [
-  {
-    "tweet": 'hi',
-    "date": 2012,
-  },
-  {
-    "tweet": 'my',
-    "date": 2018,
-  },
-  {
-    "tweet": 'teddy',
-    "date": 2018,
-  },
+    {
+        "tweet": 'hi',
+        "date": 2012,
+    },
+    {
+        "tweet": 'my',
+        "date": 2018,
+    },
+    {
+        "tweet": 'teddy',
+        "date": 2018,
+    },
 ]  # O(n^2)
+
+# Greatest number
+
+
+def greatest_number(arr):   # O(n2)
+    is_val_the_greatest = True
+    for i in arr:
+        for j in arr:
+            if j > i:
+                is_val_the_greatest = False
+        if is_val_the_greatest:
+            return i
+
+
+def greatest_number_n2(arr):  # O(n)
+    greatest_num = 0
+    for i in arr:
+        if i > greatest_num:
+            greatest_num = i
+    return greatest_num
+
+
+print(greatest_number_n2([1, 5, 3, 2]))
